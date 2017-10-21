be81379ca097bf092a6b3d3bf51e4c435735368e
Tania Leif <Tania Leif@TANIA>
2017-10-21 11:30:42 -0500

    Catálago Productos updated.

--- MainProject/MainProject/FRONTEND/frmCatalogoProductos.Designer.cs
+++ MainProject/MainProject/FRONTEND/frmCatalogoProductos.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dtgProducts = new System.Windows.Forms.DataGridView();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dtgProducts = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtgProducts)).BeginInit();
             this.SuspendLayout();
@@ -50,13 +50,48 @@
             this.panel1.Size = new System.Drawing.Size(174, 371);
             this.panel1.TabIndex = 0;
             // 
-            // dtgProducts
+            // button3
             // 
-            this.dtgProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgProducts.Location = new System.Drawing.Point(180, 12);
-            this.dtgProducts.Name = "dtgProducts";
-            this.dtgProducts.Size = new System.Drawing.Size(615, 371);
-            this.dtgProducts.TabIndex = 1;
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.button3.Location = new System.Drawing.Point(3, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exportar a Excel";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(3, 93);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(168, 39);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEditar.Enabled = false;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(3, 48);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(168, 39);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnAgregar
             // 
@@ -72,46 +107,14 @@
             this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
-            // btnEditar
+            // dtgProducts
             // 
-            this.btnEditar.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditar.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.ForeColor = System.Drawing.Color.White;
-            this.btnEditar.Location = new System.Drawing.Point(3, 48);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(168, 39);
-            this.btnEditar.TabIndex = 3;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = false;
-            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.ForeColor = System.Drawing.Color.White;
-            this.btnEliminar.Location = new System.Drawing.Point(3, 93);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(168, 39);
-            this.btnEliminar.TabIndex = 4;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = false;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.White;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.SeaGreen;
-            this.button3.Location = new System.Drawing.Point(3, 329);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(168, 39);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Exportar a Excel";
-            this.button3.UseVisualStyleBackColor = false;
+            this.dtgProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProducts.Location = new System.Drawing.Point(180, 12);
+            this.dtgProducts.Name = "dtgProducts";
+            this.dtgProducts.Size = new System.Drawing.Size(615, 371);
+            this.dtgProducts.TabIndex = 1;
+            this.dtgProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProducts_CellClick);
             // 
             // FrmCatalogoProductos
             // 
--- MainProject/MainProject/FRONTEND/frmCatalogoProductos.cs
+++ MainProject/MainProject/FRONTEND/frmCatalogoProductos.cs
@@ -15,6 +15,12 @@ namespace MainProject.FRONTEND
     public partial class FrmCatalogoProductos : Form
     {
 
+        public void data() {
+            List<ProductsPOJO> productsList = ProductsDAO.Select();
+            dtgProducts.DataSource = null;
+            dtgProducts.DataSource = productsList;
+        }
+
         public FrmCatalogoProductos()
         {
             InitializeComponent();
@@ -36,7 +42,25 @@ namespace MainProject.FRONTEND
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
+            ProductsPOJO objProducto = ProductsDAO.Select(int.Parse(dtgProducts.CurrentRow.Cells[0].Value.ToString()));
+            if (MessageBox.Show("¿Está seguro de querere eliminar el producto " + objProducto.ProductName + "?", "Eliminar producto", MessageBoxButtons.YesNo)== DialogResult.Yes)
+            {
+                if (ProductsDAO.Delete(objProducto.ProductID) == true)
+                {
+                    MessageBox.Show("Producto eliminado");
+                }
+                else
+                {
+                    MessageBox.Show("El producto no pudo ser eliminado");
+                }
+            }
 
+        }
+
+        private void dtgProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnEditar.Enabled = true;
+            btnEliminar.Enabled = true;
         }
     }
 }
